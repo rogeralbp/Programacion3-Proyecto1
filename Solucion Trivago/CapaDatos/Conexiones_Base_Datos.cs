3c82ae77be1e110cd197fe6f69965c1a75d5aa9e
punts hola mundo <anthonnyc10@gmail.com>
2018-02-13 15:47:46 -0600

    conexion

--- Solucion Trivago/CapaDatos/Conexiones_Base_Datos.cs
+++ Solucion Trivago/CapaDatos/Conexiones_Base_Datos.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace CapaDatos
 {
     public class Conexiones_Base_Datos
     {
-        //hola hola
+        static NpgsqlConnection conexion;
+        static NpgsqlCommand cmd;
+
+        public static void Conexion()
+        {
+            string servidor = "localhost";
+            int puerto = 5432;
+            string usuario = "postgres";
+            string clave = "1414250816ma";
+            string baseDatos = "gestion_vuelos";
+
+            string cadenaConexion = "Server=" + servidor + ";" + "Port=" + puerto + ";" + "User Id=" + usuario + ";" + "Password=" + clave + ";" + "Database=" + baseDatos;
+            conexion = new NpgsqlConnection(cadenaConexion);
+        }
     }
 }
