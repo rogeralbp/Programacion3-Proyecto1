ef8ce1574c4020a41fb3307667ae7a3130ee885b
ROGER ALBERTO OPORTA PEREZ <raoportap@est.utn.ac.cr>
2018-02-13 11:20:28 -0600

    cambio nombres

--- Solucion Trivago/CapaPresentacion/Form1.Designer.cs
+++ Solucion Trivago/CapaPresentacion/Form1.Designer.cs
@@ -179,7 +179,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
+            this.button2.Text = "Registrarse";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // comboBox1
