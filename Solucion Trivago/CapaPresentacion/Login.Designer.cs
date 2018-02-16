97bfa9a6cfc7df6423de27c45362a05732599c34
ROGER ALBERTO OPORTA PEREZ <raoportap@est.utn.ac.cr>
2018-02-16 13:25:59 -0600

    AGREGO IMAGENES LOGIN

--- Solucion Trivago/CapaPresentacion/Login.Designer.cs
+++ Solucion Trivago/CapaPresentacion/Login.Designer.cs
@@ -44,9 +44,13 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -56,11 +60,12 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(402, 305);
+            this.tabControl1.Size = new System.Drawing.Size(568, 395);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
             this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.txtCntraseñaLogin);
@@ -70,14 +75,14 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(394, 279);
+            this.tabPage1.Size = new System.Drawing.Size(560, 369);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Iniciar Sesion";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(311, 250);
+            this.button2.Location = new System.Drawing.Point(381, 250);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
@@ -87,7 +92,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(151, 180);
+            this.button1.Location = new System.Drawing.Point(230, 198);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -97,7 +102,7 @@
             // 
             // txtCntraseñaLogin
             // 
-            this.txtCntraseñaLogin.Location = new System.Drawing.Point(82, 138);
+            this.txtCntraseñaLogin.Location = new System.Drawing.Point(159, 161);
             this.txtCntraseñaLogin.Name = "txtCntraseñaLogin";
             this.txtCntraseñaLogin.PasswordChar = '*';
             this.txtCntraseñaLogin.Size = new System.Drawing.Size(232, 20);
@@ -106,7 +111,7 @@
             // 
             // txtCedulaLogin
             // 
-            this.txtCedulaLogin.Location = new System.Drawing.Point(82, 65);
+            this.txtCedulaLogin.Location = new System.Drawing.Point(159, 86);
             this.txtCedulaLogin.Name = "txtCedulaLogin";
             this.txtCedulaLogin.Size = new System.Drawing.Size(232, 20);
             this.txtCedulaLogin.TabIndex = 2;
@@ -115,7 +120,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(177, 106);
+            this.label2.Location = new System.Drawing.Point(227, 134);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 13);
             this.label2.TabIndex = 1;
@@ -124,7 +129,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(183, 23);
+            this.label1.Location = new System.Drawing.Point(248, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 0;
@@ -132,6 +137,7 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.pictureBox2);
             this.tabPage2.Controls.Add(this.txtContraseña);
             this.tabPage2.Controls.Add(this.txtNombre);
             this.tabPage2.Controls.Add(this.txtCedula);
@@ -142,7 +148,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(394, 279);
+            this.tabPage2.Size = new System.Drawing.Size(560, 369);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Registrarse";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -150,7 +156,7 @@
             // 
             // txtContraseña
             // 
-            this.txtContraseña.Location = new System.Drawing.Point(127, 89);
+            this.txtContraseña.Location = new System.Drawing.Point(168, 261);
             this.txtContraseña.Name = "txtContraseña";
             this.txtContraseña.Size = new System.Drawing.Size(244, 20);
             this.txtContraseña.TabIndex = 8;
@@ -158,7 +164,7 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(127, 51);
+            this.txtNombre.Location = new System.Drawing.Point(168, 204);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(244, 20);
             this.txtNombre.TabIndex = 7;
@@ -166,7 +172,7 @@
             // 
             // txtCedula
             // 
-            this.txtCedula.Location = new System.Drawing.Point(127, 12);
+            this.txtCedula.Location = new System.Drawing.Point(168, 152);
             this.txtCedula.Name = "txtCedula";
             this.txtCedula.Size = new System.Drawing.Size(244, 20);
             this.txtCedula.TabIndex = 6;
@@ -175,7 +181,7 @@
             // 
             // btnRegistrarse
             // 
-            this.btnRegistrarse.Location = new System.Drawing.Point(152, 173);
+            this.btnRegistrarse.Location = new System.Drawing.Point(254, 315);
             this.btnRegistrarse.Name = "btnRegistrarse";
             this.btnRegistrarse.Size = new System.Drawing.Size(75, 23);
             this.btnRegistrarse.TabIndex = 4;
@@ -186,7 +192,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(40, 89);
+            this.label5.Location = new System.Drawing.Point(85, 261);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(64, 13);
             this.label5.TabIndex = 2;
@@ -195,7 +201,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(40, 54);
+            this.label4.Location = new System.Drawing.Point(85, 207);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(47, 13);
             this.label4.TabIndex = 1;
@@ -204,17 +210,37 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(40, 19);
+            this.label3.Location = new System.Drawing.Point(89, 155);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 13);
             this.label3.TabIndex = 0;
             this.label3.Text = "Cedula:";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CapaPresentacion.Properties.Resources.usuario_registrados;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 141);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CapaPresentacion.Properties.Resources.r;
+            this.pictureBox2.Location = new System.Drawing.Point(205, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(161, 131);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 307);
+            this.ClientSize = new System.Drawing.Size(571, 398);
             this.Controls.Add(this.tabControl1);
             this.Name = "Login";
             this.Text = "Login";
@@ -224,6 +250,8 @@
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -246,6 +274,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
