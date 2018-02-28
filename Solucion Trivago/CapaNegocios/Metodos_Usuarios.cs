0511af27f18324af56cb4dc73aaf3bbd4b82b754
punts hola mundo <anthonnyc10@gmail.com>
2018-02-28 14:40:00 -0600

    Metodos de mostrar informacion de un vehiculo en la seccion de compra

--- Solucion Trivago/CapaNegocios/Metodos_Usuarios.cs
+++ Solucion Trivago/CapaNegocios/Metodos_Usuarios.cs
@@ -106,5 +106,168 @@ namespace CapaNegocios
 
         }
 
+
+        //Este metodo llena el comboBox de nombres de paises de origen en la seccion de vuelos
+        public void LlenarNombresPaisOrigen(ComboBox nombre_pais)
+        {
+
+            try
+            {
+                Conexion();
+                conexion.Open();
+                List<String> lista = new List<String>();
+                NpgsqlCommand cmd = new NpgsqlCommand("SELECT pais_origen FROM rutas", conexion);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        nombre_pais.Items.Add(dr.GetString(0));
+
+                    }
+                }
+                conexion.Close();
+
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+
+            }
+
+        }
+
+        //Este metodo llena el comboBox de nombres de paises de destino en la seccion de vuelos
+        public void LlenarNombresPaisDestino(ComboBox nombre_pais)
+        {
+
+            try
+            {
+                Conexion();
+                conexion.Open();
+                List<String> lista = new List<String>();
+                NpgsqlCommand cmd = new NpgsqlCommand("SELECT pais_destino FROM rutas", conexion);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        nombre_pais.Items.Add(dr.GetString(0));
+
+                    }
+                }
+                conexion.Close();
+
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+
+            }
+
+        }
+
+
+
+
+        //Este metodo llena el comboBox de nombres de hoteles en la seccion de vuelos
+        public void LlenarNombresHoteles(ComboBox hoteles)
+        {
+
+            try
+            {
+                Conexion();
+                conexion.Open();
+                List<String> lista = new List<String>();
+                NpgsqlCommand cmd = new NpgsqlCommand("SELECT nombre FROM hotel", conexion);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        hoteles.Items.Add(dr.GetString(0));
+
+                    }
+                }
+                conexion.Close();
+
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+
+            }
+
+        }
+
+        //Metodo que llena el combo de identificadores de vehiculos , en la ventana de seccion de vuelo
+        public void LlenarVehiculos(ComboBox placa)
+        {
+
+            try
+            {
+                Conexion();
+                conexion.Open();
+                List<String> lista = new List<String>();
+                NpgsqlCommand cmd = new NpgsqlCommand("SELECT placa FROM vehiculos", conexion);
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        placa.Items.Add(dr.GetInt64(0));
+                    }
+                }
+                conexion.Close();
+
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+
+            }
+
+        }
+
+   
+        //Este metodo muestra la informacion del vehiculo seleccionado en el combobox por el usuario
+        public void Combo2Vehiculos(ComboBox placa, TextBox marca, TextBox modelo, TextBox tipo_vehiculo, TextBox precio)
+        {
+
+            try
+            {
+                Conexion();
+                conexion.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("SELECT marca, modelo , tipo_vehiculo , precio FROM vehiculos WHERE placa = '" + placa.SelectedItem.ToString() + "'", conexion);
+                NpgsqlDataReader leer = cmd.ExecuteReader();
+                if (leer.HasRows)
+                {
+                    while (leer.Read())
+                    {
+                        marca.Text = leer.GetString(0);
+                        modelo.Text = leer.GetString(1);
+                        tipo_vehiculo.Text = leer.GetString(2);
+                        precio.Text = leer.GetDouble(3).ToString();
+                       
+                    }
+                    conexion.Close();
+
+                }
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error);
+            }
+
+        }
+
+
     }
+
+
 }
